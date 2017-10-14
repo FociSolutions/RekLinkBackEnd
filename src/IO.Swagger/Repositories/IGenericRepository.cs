c45659147f9b823a14a8ca044b121460d3c9d933
dan.mccrady <dan.mccrady@focisolutions.com>
2017-10-14 13:45:33 -0400

    Adding new files and repositories

---
+++ src/IO.Swagger/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IO.Swagger.Repositories
+{
+    interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
