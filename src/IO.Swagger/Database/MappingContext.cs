301ddf2660a7a74631eb6d160ea8b173642c68ae
dan.mccrady <dan.mccrady@focisolutions.com>
2017-10-14 12:55:51 -0400

    Updating project references

---
+++ src/IO.Swagger/Database/MappingContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using IO.Swagger.Models;
+
+namespace IO.Swagger
+{
+    public partial class MappingContext : DbContext
+    {
+        public MappingContext(DbContextOptions<MappingContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql(@"Host=horton.elephantsql.com;Database=robzbgrz;Username=robzbgrz;Password=pzTBhdLTBryMg7Y-8oDfnZ3WEHyv1ZGZ");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("btree_gin")
+                .HasPostgresExtension("btree_gist")
+                .HasPostgresExtension("citext")
+                .HasPostgresExtension("cube")
+                .HasPostgresExtension("dblink")
+                .HasPostgresExtension("dict_int")
+                .HasPostgresExtension("dict_xsyn")
+                .HasPostgresExtension("earthdistance")
+                .HasPostgresExtension("fuzzystrmatch")
+                .HasPostgresExtension("hstore")
+                .HasPostgresExtension("intarray")
+                .HasPostgresExtension("ltree")
+                .HasPostgresExtension("pg_stat_statements")
+                .HasPostgresExtension("pg_trgm")
+                .HasPostgresExtension("pgcrypto")
+                .HasPostgresExtension("pgrowlocks")
+                .HasPostgresExtension("pgstattuple")
+                .HasPostgresExtension("plv8")
+                .HasPostgresExtension("tablefunc")
+                .HasPostgresExtension("unaccent")
+                .HasPostgresExtension("uuid-ossp")
+                .HasPostgresExtension("xml2");
+        }
+
+
+        /// <summary>
+        /// Mapping data dbset
+        /// </summary>
+        public DbSet<MapPoint> MapPoints { get; set; }
+    }
+}
