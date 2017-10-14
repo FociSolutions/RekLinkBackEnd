bb892f03f8fe4b1240350088d69c36b2ccdc5e01
dan.mccrady <dan.mccrady@focisolutions.com>
2017-10-14 13:31:12 -0400

    Connect to cloud database. Initial Migration added.

---
+++ src/IO.Swagger/Database/MapPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IO.Swagger.Database
+{
+    public class MapPoint
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public ICollection<MetaData> MetaData { get; set; }
+
+    }
+}
---
+++ src/IO.Swagger/Database/MetaData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IO.Swagger.Database
+{
+    public class MetaData
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public MapPoint MapPoint { get; set; }
+    }
+}
