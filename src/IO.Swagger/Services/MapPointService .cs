f7de71e7980bf03aab18d0faf92d0bb63b6863a7
Rabie Almatarneh <rabie.almatarneh@focisolutions.com>
2017-10-14 15:07:22 -0400

    MapPointService Inti commit

---
+++ src/IO.Swagger/Services/MapPointService .cs
@@ -0,0 +1,28 @@
+﻿using Geocoding;
+using Geocoding.Google;
+using System;
+
+namespace IO.Swagger.Services
+{
+    public class MapPointService
+    {
+        IGeocoder geoCoder;
+
+        public IGeocoder CreateAsyncGeocoder()
+        {
+            string apiKey = string.Empty; // settings.GoogleApiKey;
+
+            if (String.IsNullOrEmpty(apiKey))
+            {
+                geoCoder = new GoogleGeocoder();
+            }
+            else
+            {
+                geoCoder = new GoogleGeocoder(apiKey);
+            }
+
+            return geoCoder;
+        }
+
+    }
+}
