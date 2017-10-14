607ba9cee2040a36c1767f782a08bd59ad791961
Rabie Almatarneh <rabie.almatarneh@focisolutions.com>
2017-10-14 15:12:55 -0400

    Update the geo map service.

---
+++ src/IO.Swagger/Services/IMapPointService .cs
@@ -0,0 +1,13 @@
+﻿using Geocoding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IO.Swagger.Repositories
+{
+    interface IMapPointService
+    {
+        IGeocoder CreateAsyncGeocoder();
+    }
+}
