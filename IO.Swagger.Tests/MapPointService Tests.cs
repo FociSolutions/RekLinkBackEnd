5baa4d9506f3815452a869a26b67246cd35ea752
Rabie Almatarneh <rabie.almatarneh@focisolutions.com>
2017-10-15 10:25:01 -0400

    Added unit tests project

---
+++ IO.Swagger.Tests/MapPointService Tests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace IO.Swagger.Tests
+{
+    public class MapPointServiceTests
+    {
+        [Fact]
+        [Trait("Category", "Success")]
+        public void TestGetFencedData()
+        {
+            //TODO
+        }
+
+        [Fact]
+        [Trait("Category", "Success")]
+        public void TestAddMapdData()
+        {
+            //TODO
+        }
+    }
+}
